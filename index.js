dfb9e66bd891c6aad70a8c381765d3dc9f2d80a7
blodienn <160183783+blodienn@users.noreply.github.com>
2024-02-16 19:45:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('Ay\nPapi\nKchame', {
+say('MK\nBLODIE\n2', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`OPTIMUS BOT`, {
+say(`BLODIE 2`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
