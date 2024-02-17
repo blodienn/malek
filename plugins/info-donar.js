bbef9d3e14336ca849695c523e97878e624a4dc8
blodienn <160183783+blodienn@users.noreply.github.com>
2024-02-16 23:26:20 -0300

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -2,9 +2,9 @@ let media = './media/menus/Menu4paypal.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
   let user = db.data.users[m.sender]
-let str = `𝗟𝗼𝗻𝗴 𝗧𝗶𝗺𝗲 𝗕𝗼𝘁
+let str = `BLODIE II 𝗕𝗼𝘁
 
-𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗗𝗼𝗻𝗮𝗿 𝗘𝘀𝗰𝗿𝗶𝗯𝗲 𝗔𝗹 𝗦𝗶𝗴𝘂𝗶𝗲𝗻𝘁𝗲 𝗡𝘂𝗺𝗲𝗿𝗼 : +51 936 994 155`
+𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗗𝗼𝗻𝗮𝗿 𝗘𝘀𝗰𝗿𝗶𝗯𝗲 𝗔𝗹 𝗦𝗶𝗴𝘂𝗶𝗲𝗻𝘁𝗲 𝗡𝘂𝗺𝗲𝗿𝗼 : +54 9 11 5963-4868`
 await conn.sendMessage(m.chat, { 
 text: str, 
 contextInfo:{
@@ -15,7 +15,7 @@ mentionedJid:[m.sender],
 "showAdAttribution": true,
 "renderLargerThumbnail": true,
 "thumbnail": imagen1, 
-"title": `𝗟𝗼𝗻𝗴 𝗧𝗶𝗺𝗲 𝗕𝗼𝘁`, 
+"title": `BLODIE II 𝗕𝗼𝘁`, 
 "containsAutoReply": true,
 "mediaType": 1, 
 "mediaUrl": paypal, 
